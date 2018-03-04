aefcbad99e692e1291d3543845678adb152b9acc
Kelsey Fecho <kfecho30@gmail.com>
2018-03-04 21:48:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each |combo| do
+  WIN_COMBINATIONS.each do |combo| 
     if board[combo[0]] == board[combo[1]] && board[combo[0]] == board[combo[2]]
       true
     else
